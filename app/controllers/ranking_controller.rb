223091c637f73d72de970aa741332378d500162f
Takayuki Hirayama <hirayama@fiilse.com>
2016-07-12 21:48:30 +0900

    q2

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -2,7 +2,12 @@ class RankingController < ApplicationController
 	
 	
 	def have
-		Have.group(:item_id).order(:item_id).limit(10).count(:Item_id :desc)
+		@ids = Have.group(:item_id).order('count_item_id desc').limit(10).count(:Item_id).to_a
+		@items = []
+		@ids.each do |id|
+			@items << Item.find(id[0])
+		end
+		
 	end
 
 
