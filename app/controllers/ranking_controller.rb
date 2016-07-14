1ade167d15b22f3f8ab015b3366b010043708640
takayuki <hirayama@fiilse.com>
2016-07-14 10:14:33 +0000

    mentored

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -3,11 +3,16 @@ class RankingController < ApplicationController
 	
 	def have
 		@ids = Have.group(:item_id).order('count_item_id desc').limit(10).count(:Item_id).to_a
+		#@ids = Have.group(:item_id).order('count_item_id desc').limit(10).count(:Item_id).keys
 		@items = []
 		@ids.each do |id|
 			@items << Item.find(id[0])
+			# @items << item.find(id)
 		end
 		
+# 	@items = @id.map do |id|
+# 	   Item.find(id)
+# 	end		
 	end
 
 
