a60450b1aaeeb41a39c29bef3c6b0bc025e9db34
Takayuki Hirayama <hirayama@fiilse.com>
2016-07-14 20:23:37 +0900

    faze1

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,23 +1,25 @@
 class RankingController < ApplicationController
-	
-	
-	def have
-		@ids = Have.group(:item_id).order('count_item_id desc').limit(10).count(:Item_id).to_a
-		#@ids = Have.group(:item_id).order('count_item_id desc').limit(10).count(:Item_id).keys
-		@items = []
-		@ids.each do |id|
-			@items << Item.find(id[0])
-			# @items << item.find(id)
-		end
-		
-# 	@items = @id.map do |id|
-# 	   Item.find(id)
-# 	end		
-	end
+  def have
+    @ids = Have.group(:item_id).order('count_item_id desc').limit(10).count(:Item_id).to_a
+    # @ids = Have.group(:item_id).order('count_item_id desc').limit(10).count(:Item_id).keys
+    @items = []
+    @ids.each do |id|
+      @items << Item.find(id[0])
+      # @items << item.find(id)
+    end
 
+    # 	@items = @id.map do |id|
+    # 	   Item.find(id)
+    # 	end
+  end
 
-
-	def want
-		
-	end
+  def want
+    @ids = Want.group(:item_id).order('count_item_id desc').limit(10).count(:Item_id).to_a
+    # @ids = Have.group(:item_id).order('count_item_id desc').limit(10).count(:Item_id).keys
+    @items = []
+    @ids.each do |id|
+      @items << Item.find(id[0])
+      # @items << item.find(id)
+    end
+  end
 end
